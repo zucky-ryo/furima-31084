64a4eff9fba1edbea6c6f735da211de0e66cbcba
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-13 11:06:40 +0900

    ストロングパラメーターの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameter, if: :devise_controller?
+
+  private
+  def configure_permitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_kana, :first_kana, :birthday])
+  end
 end
