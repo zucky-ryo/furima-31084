2be1900d0133aa9c186cc31a725be83190800837
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-13 20:21:29 +0900

    itemsテーブルのバリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,15 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :days
 
+  validates :name,       presence: true
+  validates :text,       presence: true
+  validates :category,   presence: true
+  validates :status,     presence: true
+  validates :charge,     presence: true
+  validates :prefecture, presence: true
+  validates :days,       presence: true
+  validates :price,      presence: true
+
   validates :category_id,   numericality: { other_than: 1 }
   validates :status_id,     numericality: { other_than: 1 }
   validates :charge_id,     numericality: { other_than: 1 }
