89af5cb65b56f52b9d7bfc0fb98de31849b4e354
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-13 19:40:36 +0900

    各モデルのActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :days
+
+  validates :category_id,   numericality: { other_than: 1 }
+  validates :status_id,     numericality: { other_than: 1 }
+  validates :charge_id,     numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :days_id,       numericality: { other_than: 1 }
 end
