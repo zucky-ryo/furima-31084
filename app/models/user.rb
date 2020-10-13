d8e380913adf9d2d82349840b309838aaef998fe
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-13 12:35:20 +0900

    名前入力部分のバリデーションの再実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,    presence: true
-  validates :family_name, presence: true
-  validates :first_name,  presence: true
-  validates :family_kana, presence: true
-  validates :first_kana,  presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+  validates :first_name,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+  validates :family_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
+  validates :first_kana,  presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
   validates :birthday,    presence: true
+
 end
