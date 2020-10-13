9d65c3c61d041ba2473ecfac539e9c0339c12c69
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-13 17:08:17 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,4 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-
 end
