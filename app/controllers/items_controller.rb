5870c3805efbe417b0785f0c1ca95bea979556c1
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-14 12:54:44 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :charge_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
   end
 end
