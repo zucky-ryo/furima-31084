3ecc79a766db4bc029d6329fc80c3a6e2d1115fe
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-14 17:10:32 +0900

    JavaScriptの実装

---
+++ app/javascript/items/new.js
@@ -0,0 +1,15 @@
+function calc() {
+  const form = document.getElementById("item-price");
+  const tax = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  form.addEventListener('input', () => {
+    let price = parseInt(form.value, 10);
+    let taxRes = parseInt(price / 10);
+    let profitRes = parseInt(price / 10 * 9);
+    tax.innerHTML = taxRes.toLocaleString();
+    profit.innerHTML = profitRes.toLocaleString();
+  });
+}
+
+window.addEventListener('load', calc);
