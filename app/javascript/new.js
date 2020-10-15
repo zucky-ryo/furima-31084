d23521a5be7732fbfc9111ed45c5df52d51c380c
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-15 19:21:59 +0900

    デバッグ用コードの削除

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,7 +1,6 @@
 window.addEventListener('turbolinks:load', () => {
   let urlPath = location.pathname;
   if (urlPath.match(/items/)) {
-    console.log("aaa")
     const form = document.getElementById("item-price");
     const tax = document.getElementById("add-tax-price");
     const profit = document.getElementById("profit");
