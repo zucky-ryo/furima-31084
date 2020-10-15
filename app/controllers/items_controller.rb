3bb1f163e063a08de67b39f9687cd6d80c21ec3d
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-15 20:19:59 +0900

    商品情報削除の条件を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if current_user == @item.user
+      @item.destroy
+    end
     redirect_to root_path
   end
 
