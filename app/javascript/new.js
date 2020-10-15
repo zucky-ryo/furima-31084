daa3b23e1eb9013f9fcd7a0c5263498560d7c579
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-15 11:24:30 +0900

    JavaScriptの修正

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,6 +1,6 @@
 window.addEventListener('turbolinks:load', () => {
   let urlPath = location.pathname;
-  if (urlPath == '/items/new') {
+  if (urlPath == '/items/new' || urlPath == '/items') {
     const form = document.getElementById("item-price");
     const tax = document.getElementById("add-tax-price");
     const profit = document.getElementById("profit");
