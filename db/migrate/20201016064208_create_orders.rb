082306198849a912accc80ed9f71fec78b02e61e
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-16 15:55:55 +0900

    addressesテーブルの実装

--- db/migrate/20201016064208_create_orders.rb
+++ db/migrate/20201016064208_create_orders.rb
@@ -3,6 +3,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
     create_table :orders do |t|
       t.references :user, foreign_key: true
       t.references :item, foreign_key: true
+      
       t.timestamps
     end
   end
