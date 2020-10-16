9a43d181676c34d8c6a656ea574e207006d81791
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-16 17:59:49 +0900

    addressesテーブルのバリデーションの実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,16 @@
 class Address < ApplicationRecord
   belongs_to :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+  with_options presence: true do
+    validates :post_code
+    validates :prefecture
+    validates :city
+    validates :address
+    validates :phone
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1 }
 end
