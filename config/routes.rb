541c5f75d6a85c67c491d480470df11d2f9696b5
zucky-ryo <ryota980117@ezweb.ne.jp>
2020-10-16 17:41:53 +0900

    購入情報ページの購入物の表示の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: 'items#index'
-  resources :items
-  resources :orders, only: [:index, :create]
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
   
 end
